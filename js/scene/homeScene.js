712c88e4b259d9b1dedf4b0591a4b998ed97568b
YuLiang <liang.yu@kaiostech.com>
2019-04-25 05:47:44 +0800

    avatar and start ok

--- js/scene/homeScene.js
+++ js/scene/homeScene.js
@@ -1,7 +1,6 @@
 import Background from '../runtime/background'
 import DataStore from '../base/DataStore';
 import Sprite from '../base/Sprite';
-import { createUserInfoButton } from '../utils/auth.js';
 
 const screenWidth = window.innerWidth;
 const screenHeight = window.innerHeight;
@@ -11,122 +10,124 @@ export default class HomeScene {
     this.ctx = ctx;
     this.canvas = DataStore.getInstance().canvas;
     // this.loop();
-    this.getSettingAndDrawAvatarAndStartButton();
+    // this.getSettingAndDrawAvatarAndStartButton();
+
+
+    this.drawBackground()
+    this.getSettingAndDrawAvatar()
+    this.drawStartButton()
   }
 
-  getSettingAndDrawAvatarAndStartButton = () => {
-    let self = this;
+  getSettingAndDrawAvatar() {
+    this.wxgetSetting()
+  }
+
+  wxgetSetting() {
+    let self = this
     wx.getSetting({
       success(res) {
-        let authSetting = res.authSetting;
-        if (authSetting['scope.userInfo'] === true) {
-          // 用户已授权，可以直接调用相关 API
-          wx.getUserInfo({
-            withCredentials: true,
-            success: (res) => {
-              self.userInfo = res.userInfo;
-              console.log(`getUserInfo success`, res.userInfo)
-              // self.drawAvatar()
-              // self.drawStartButton()
-              self.loop()
-            },
-          });
+        console.log(`getSetting success: `, res)
+        if (res.authSetting['scope.userInfo'] === true) {
+          self.wxgetUserInfo()
         } else {
-          // self.drawAvatar()
-          // self.drawStartButton()
-          self.loop()
+          self.drawAvatar()
         }
-        console.log(`authSetting['scope.userInfo']: `, authSetting['scope.userInfo'])
-
-        // else if (authSetting['scope.userInfo'] === false) {
-        //   // 用户已拒绝授权，再调用相关 API 或者 wx.authorize 会失败，需要引导用户到设置页面打开授权开关
-        //   console.log('到设置页面打开授权开关');
-        //   // wx.showModal({
-        //   //   title: '提示',
-        //   //   content: '请到设置页面打开授权开关',
-        //   //   showCancel: false,
-        //   //   confirmText: '知道了',
-        //   //   success: res => {
-        //   //   }
-        //   // });
-        // } else {
-        //   // 未询问过用户授权，调用相关 API 或者 wx.authorize 会弹窗询问用户
-        //   wx.authorize({
-        //     scope: 'scope.userInfo',
-        //     success: res => {
-        //       _this.dataStore.userInfo = res.userInfo;
-        //       // console.log(_this.dataStore.userInfo);
-        //     },
-        //     fail: res => {
-        //       // iOS 和 Android 对于拒绝授权的回调 errMsg 没有统一，需要做一下兼容处理
-        //       if (res.errMsg.indexOf('auth deny') > -1 || res.errMsg.indexOf('auth denied') > -1) {
-        //         // 处理用户拒绝授权的情况
-        //       }
-        //     }
-        //   })
-        // }
-
-
       }
     })
+  }
 
-    // self.drawAvatar()
-    // self.drawStartButton()
+  wxgetUserInfo() {
+    let self = this
+    wx.getUserInfo({
+      success(res) {
+        console.log(`getUserInfo success: `, res)
+        self.userInfo = res.userInfo
+        self.wxdownloadFile(self.userInfo.avatarUrl)
+      }
+    })
+  }
+
+  wxdownloadFile(url) {
+    let self = this
+    wx.downloadFile({
+      url: url,
+      success(res) {
+        console.log(`downloadFile success: `, res)
+        if (res.statusCode === 200) {
+          self.wxsaveFile(res.tempFilePath)
+        }
+      }
+    })
+  }
+
+  wxsaveFile(url) {
+    let self = this
+    wx.saveFile({
+      tempFilePath: url,
+      success(res) {
+        console.log(`saveFile success: `, res)
+        self.userInfo.savedFilePath_avatar = res.savedFilePath;
+        self.drawAvatar()
+      }
+    })
+  }
+
+
+  drawBackground() {
+    this.ctx.clearRect(0, 0, screenWidth, screenHeight);
+    this.background = new Background(this.ctx);
   }
 
   drawAvatar() {
-    // if (this.userInfo) {
-    //   let self = this;
-    //   this.homeEle = wx.createImage();
-    //   this.homeEle.src = this.userInfo.avatarUrl;
-    //   this.homeImg = new Sprite(this.homeEle, screenWidth / 3, screenHeight / 4, screenWidth / 3, screenWidth / 3);
-    //   this.homeEle.onload = function () {
-    //     self.homeImg.draw(self.ctx);
-    //   }
-    // } else
-    {
-      this.homeEle = Sprite.getImage(this.userInfo ? 'pfc_local' : 'blank_avatar');
+    if (this.userInfo && this.userInfo.savedFilePath_avatar) {
+      let self = this;
+      this.homeEle = wx.createImage();
+      this.homeEle.src = this.userInfo.savedFilePath_avatar;
+      this.homeImg = new Sprite(this.homeEle, screenWidth / 3, screenHeight / 4, screenWidth / 3, screenWidth / 3);
+      this.homeEle.onload = function () {
+        self.homeImg.draw(self.ctx);
+      }
+    } else {
+      // this.homeEle = Sprite.getImage(this.userInfo ? 'pfc_local' : 'blank_avatar');
+      this.homeEle = Sprite.getImage('blank_avatar');
       this.homeImg = new Sprite(this.homeEle, screenWidth / 3, screenHeight / 4, screenWidth / 3, screenWidth / 3);
       this.homeImg.draw(this.ctx)
     }
+
     console.log(`homeEle: `, this.homeEle)
-    this.logoImg = Sprite.getImage('logo');
-    console.log(`logoImg: `, this.logoImg)
-
-
+    // this.logoImg = Sprite.getImage('logo');
+    // console.log(`logoImg: `, this.logoImg)
   }
 
   drawStartButton() {
-    this.btnImg = Sprite.getImage('start_btn');
-    this.startSprite = new Sprite(this.btnImg, (screenWidth - this.btnImg.width / 2) / 2, screenHeight / 2,
-      this.btnImg.width / 2, this.btnImg.height / 2);
-    this.startSprite.draw(this.ctx);
-
-    this.rankImg = Sprite.getImage('rank_btn');
-    this.rankSprite = new Sprite(this.rankImg, (screenWidth - this.rankImg.width / 2) / 2, this.startSprite.y + this.startSprite.height + 20,
-      this.rankImg.width / 2, this.rankImg.height / 2);
-    this.rankSprite.draw(this.ctx);
-
-    this.bindEvent();
+    var button = wx.createUserInfoButton({
+      type: 'text',
+      text: 'Start',
+      style: {
+        left: screenWidth / 3,
+        top: screenHeight / 2,
+        width: screenWidth / 3,
+        height: screenHeight / 20,
+        lineHeight: 40,
+        backgroundColor: '#ff0000',
+        color: '#ffffff',
+        textAlign: 'center',
+        fontSize: 16,
+        borderRadius: 4
+      }
+    });
+    // button.show();
+    let self = this;
+    button.onTap((res) => {
+      // console.log(`createUserInfoButton res: `, res)
+      console.log(`onTap res: `, res)
+      if (res.userInfo) {
+        DataStore.getInstance().director.toQuestionScene(self.ctx);
+        button.destroy();
+      }
+    });
   }
 
-  loop() {
-    // console.log(`loop`)
-    this.ctx.clearRect(0, 0, screenWidth, screenHeight);
-    this.background = new Background(this.ctx);
-    this.drawAvatar();
-    // this.drawStartButton();
-    createUserInfoButton(this.ctx)
-    // if (DataStore.getInstance().shareTicket && !this.showGroup) {
-    //   this.showGroup = true;
-    //   this.messageSharecanvas('group', DataStore.getInstance().shareTicket);
-    // }
-    // if (this.ranking) {
-    //   // 子域canvas 放大绘制，这里必须限制子域画到上屏的宽高是screenWidth， screenHeight
-    //   DataStore.getInstance().ctx.drawImage(DataStore.getInstance().sharedCanvas, 0, 0, screenWidth, screenHeight);
-    // }
-    // this.requestId = requestAnimationFrame(this.loop.bind(this));
-  }
 
   messageSharecanvas(type, text) {
     // 排行榜也应该是实时的，所以需要sharedCanvas 绘制新的排行榜
@@ -144,7 +145,7 @@ export default class HomeScene {
     if (this.ranking) {
       wx.onTouchStart((e) => {
         let x = e.touches[0].clientX,
-          y = e.touches[0].clientY;
+            y = e.touches[0].clientY;
         let scale = screenWidth / 750;
         if (x >= 80 * scale && x <= 180 * scale && y >= 1120 * scale && y <= 12200 * scale) {// 返回按钮
           console.log(`onTouchStart 1 return??`)
@@ -158,11 +159,11 @@ export default class HomeScene {
     }
     wx.onTouchStart((e) => {
       let x = e.touches[0].clientX,
-        y = e.touches[0].clientY;
+          y = e.touches[0].clientY;
       if (x >= _this.startSprite.x
-        && x <= _this.startSprite.x + _this.startSprite.width
-        && y >= _this.startSprite.y
-        && y <= _this.startSprite.y + _this.startSprite.height) {
+          && x <= _this.startSprite.x + _this.startSprite.width
+          && y >= _this.startSprite.y
+          && y <= _this.startSprite.y + _this.startSprite.height) {
         console.log(`onTouchStart 2 toQuestionScene`)
         cancelAnimationFrame(_this.requestId);
         if (!this.userInfo) {
@@ -187,9 +188,9 @@ export default class HomeScene {
           DataStore.getInstance().director.toQuestionScene(_this.ctx);
         }
       } else if (x >= _this.rankSprite.x
-        && x <= _this.rankSprite.x + _this.rankSprite.width
-        && y >= _this.rankSprite.y
-        && y <= _this.rankSprite.y + _this.rankSprite.height) {
+          && x <= _this.rankSprite.x + _this.rankSprite.width
+          && y >= _this.rankSprite.y
+          && y <= _this.rankSprite.y + _this.rankSprite.height) {
         // 排行榜也应该是实时的，所以需要sharedCanvas 绘制新的排行榜
         console.log(`onTouchStart 3 messageSharecanvas`)
         _this.messageSharecanvas();
